97e6763d449ffc4f63290f5f86c287f4b6da300d
zhangjinci <throwable@qq.com>
2024-09-12 20:59:03 +0800

    fix: fix connection pool options

--- solpic-core/src/main/java/cn/vlts/solpic/core/codec/impl/JacksonCodec.java
+++ solpic-core/src/main/java/cn/vlts/solpic/core/codec/impl/JacksonCodec.java
@@ -3,6 +3,7 @@ package cn.vlts.solpic.core.codec.impl;
 import cn.vlts.solpic.core.codec.Codec;
 import cn.vlts.solpic.core.util.ReflectionUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Type;
+import java.util.Objects;
 
 /**
  * Jackson codec.
@@ -19,12 +21,12 @@ import java.lang.reflect.Type;
  */
 public class JacksonCodec<S, T> implements Codec<S, T> {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private volatile ObjectMapper objectMapper;
 
     @Override
     public byte[] toByteArray(S s) {
         try {
-            return objectMapper.writeValueAsBytes(s);
+            return getObjectMapper().writeValueAsBytes(s);
         } catch (JsonProcessingException e) {
             throw new IllegalStateException("Failed to serialize object", e);
         }
@@ -33,8 +35,8 @@ public class JacksonCodec<S, T> implements Codec<S, T> {
     @Override
     public T fromByteArray(byte[] bytes, Type targetType) {
         try {
-            JavaType javaType = objectMapper.getTypeFactory().constructType(targetType);
-            return objectMapper.readValue(bytes, javaType);
+            JavaType javaType = getObjectMapper().getTypeFactory().constructType(targetType);
+            return getObjectMapper().readValue(bytes, javaType);
         } catch (Exception e) {
             throw new IllegalStateException("Failed to deserialize object", e);
         }
@@ -42,18 +44,30 @@ public class JacksonCodec<S, T> implements Codec<S, T> {
 
     @Override
     public int write(OutputStream outputStream, S s) throws IOException {
-        objectMapper.writeValue(outputStream, s);
+        getObjectMapper().writeValue(outputStream, s);
         return -1;
     }
 
     @Override
     public T read(InputStream inputStream, Type targetType) throws IOException {
         try {
-            JavaType javaType = objectMapper.getTypeFactory().constructType(targetType);
-            return objectMapper.readValue(inputStream, javaType);
+            JavaType javaType = getObjectMapper().getTypeFactory().constructType(targetType);
+            return getObjectMapper().readValue(inputStream, javaType);
         } catch (Exception e) {
             throw new IllegalStateException("Failed to deserialize object", e);
         }
+    }
+
+    private ObjectMapper getObjectMapper() {
+        if (Objects.isNull(this.objectMapper)) {
+            synchronized (this) {
+                if (Objects.isNull(this.objectMapper)) {
+                    this.objectMapper = new ObjectMapper();
+                    this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                }
+            }
+        }
+        return this.objectMapper;
     }
 
     static {
